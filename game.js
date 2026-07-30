5edcceb352fa8e7eb7bbfb52bb55d631ae7321b3
csboost-id <asrockjrc@gmail.com>
2026-07-31 03:43:01 +0700

    Update game.js

--- game.js
+++ game.js
@@ -1,66 +1,58 @@
 const config = {
     type: Phaser.AUTO,
+
     width: 800,
     height: 600,
-    backgroundColor: "#4a8f4a",
+
+    backgroundColor: "#222222",
+
     physics: {
         default: "arcade",
         arcade: {
             debug: false
         }
     },
+
     scene: {
-        preload,
         create,
         update
     }
 };
 
+
 const game = new Phaser.Game(config);
 
+
 let player;
-let cursors;
 let keys;
 
-function preload() {
-}
 
 function create() {
 
-
-    // Membuat dunia rumput
+    // Dunia rumput
     this.add.rectangle(
-    400,
-    300,
-    800,
-    600,
-    0x5aa05a
-);
-
-    grass.setDepth(0);
+        400,
+        300,
+        800,
+        600,
+        0x5aa05a
+    );
 
 
-    player = this.add.rectangle(400, 300, 32, 32, 0x3498db);
+    // Player
+    player = this.add.rectangle(
+        400,
+        300,
+        32,
+        32,
+        0x3498db
+    );
 
-    player.setDepth(1);
 
     this.physics.add.existing(player);
 
 
-    // Ukuran dunia
-this.physics.world.setBounds(0, 0, 800, 600);
-
-player.body.setCollideWorldBounds(true);
-
-
-// Kamera mengikuti player
-// Kamera dimatikan dulu untuk test
-// this.cameras.main.startFollow(player);
-// this.cameras.main.setBounds(0, 0, 800, 600);
-// this.cameras.main.setZoom(1);
-
-    cursors = this.input.keyboard.createCursorKeys();
-
+    // Keyboard
     keys = this.input.keyboard.addKeys({
         W: Phaser.Input.Keyboard.KeyCodes.W,
         A: Phaser.Input.Keyboard.KeyCodes.A,
@@ -68,24 +60,40 @@ player.body.setCollideWorldBounds(true);
         D: Phaser.Input.Keyboard.KeyCodes.D
     });
 
+
 }
+
 
 function update() {
 
+
     player.body.setVelocity(0);
+
 
     const speed = 200;
 
-    if (keys.A.isDown || cursors.left.isDown)
+
+    if (keys.A.isDown)
+    {
         player.body.setVelocityX(-speed);
+    }
 
-    if (keys.D.isDown || cursors.right.isDown)
+
+    if (keys.D.isDown)
+    {
         player.body.setVelocityX(speed);
+    }
 
-    if (keys.W.isDown || cursors.up.isDown)
+
+    if (keys.W.isDown)
+    {
         player.body.setVelocityY(-speed);
+    }
 
-    if (keys.S.isDown || cursors.down.isDown)
+
+    if (keys.S.isDown)
+    {
         player.body.setVelocityY(speed);
+    }
 
 }
